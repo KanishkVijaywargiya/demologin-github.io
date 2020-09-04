9c16b56db9c26d200bcd1c964136d768af5c3b35
Kanishk <kanishkvijaywargiya99@gmail.com>
2020-09-04 23:19:52 +0530

    added extra space between button and footer, linked header logo to github as well

--- js/func.js
+++ js/func.js
@@ -16,9 +16,15 @@ function validate() {
         srn.search(/^[A-Z a-z]+[0-9]+[0-9]+[A-Z a-z]+[A-Z a-z]+[0-9]+[0-9]+[0-9]+$/) == -1 ||
         srn.length != 8) {
         failed.style.display = "block";
-        setTimeout(() => { hideFailed() }, 3000); // 3 seconds
+        console.log('failed:', srn);
+        setTimeout(() => {
+            hideFailed()
+        }, 3000);
     } else {
         success.style.display = "block";
-        setTimeout(() => { hideSuccess() }, 3000); // 3 seconds
+        setTimeout(() => {
+            hideSuccess()
+        }, 3000);
+        console.log('success:', srn);
     }
 };
